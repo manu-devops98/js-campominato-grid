32d94a80372e6b7bd9333013b81afb1d653913f0
manu-devops98 <manuelcappello7@gmail.com>
2021-12-01 17:57:54 +0100

    add script

--- js/script.js
+++ js/script.js
@@ -2,37 +2,47 @@
 const container = document.querySelector('.container');
 const button = document.getElementById('play-button');
 // console.log(button);
-const select = document.getElementById('difficulty').value;
+const select = document.getElementById('difficulty');
 // console.log(select);
 
 let row = 0;
 let col = 0;
-const numberBox = row * col;
 
 button.addEventListener('click',
 function () {
-    
-    if (select == 'easy') {
+    container.innerHTML = '';
+    if (select.value == 'easy') {
         row = 10;
         col = 10;
-    } else if (select == 'hard') {
+        console.log(select.value);
+    } else if (select.value == 'hard') {
         row = 9;
         col = 9;
-    } else if (select == 'crazy') {
+        console.log(select.value);
+    } else if (select.value == 'crazy') {
         row = 7;
         col = 7;
+        console.log(select.value);
     }
+    
+    const numberBox = row * col;
+
+    for (let i = 0; i < numberBox; i++) {
+        const square = document.createElement('div');
+        square.classList.add('box');
+        square.style.width = `calc(100% / ${col})`;
+        square.style.height = `calc(100% / ${row})`;
+        square.append(i + 1);
+        console.log(square);
+        container.append(square); 
+        
+        square.addEventListener('click', function () {
+           square.classList.add('clicked'); 
+        });
+    }
+
 });
 
-for (let i = 0; i < numberBox; i++) {
-    const square = document.createElement('div');
-    square.classList.add('box');
-    square.style.width = `calc(100% / ${col})`;
-    square.style.height = `calc(100% / ${row})`;
-    square.append(i + 1);
-    container.append(square);
-    
-}
 
 
 
